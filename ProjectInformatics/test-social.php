88765bc0ace56acf99d6e3c74ec9ec663cbf4171
manatsawi <manatsawi.sut@gmail.com>
2017-07-06 03:26:50 +0700

    netapp hci

---
+++ ProjectInformatics/test-social.php
@@ -0,0 +1,20 @@
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><div class="session1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style media="screen">
+  <?php include_once 'css/frontawesome.css'; ?>
+</style>
+  <div class="row ">
+  <h6 class="display-6">Sripatum University @Informatics</h6>
+      <a href="#" class="fa fa-facebook fa-custom"></a>
+      <a href="#" class="fa fa-google fa-custom"></a>
+      <a href="#" class="fa fa-linkedin fa-custom"></a>
+      <a href="#" class="fa fa-youtube fa-custom"></a>
+      <a href="#" class="fa fa-instagram fa-custom"></a>
+  </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
